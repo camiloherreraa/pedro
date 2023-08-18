5737be514306fa9c354c45e17236e2a0495d09f7
camiloherreraa <camilo.arcila97@gmail.com>
2023-08-18 15:18:00 -0500

    este es mi primer commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Este script imprime los numeros enteros
---
+++ pedro.py
@@ -0,0 +1,6 @@
+# Imprimir los numeros pares
+numFinal = int(input('Ingrese numero final'))
+listPares = []
+for i in range(2,numFinal,2):
+    listPares.append(i)
+print('Estos son los pares', listPares)
